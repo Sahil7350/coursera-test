98d1c091f2bfeb34feae8ba97640b6d763ab36ff
Sahil Sidhani <67865192+Sahil7350@users.noreply.github.com>
2020-07-18 18:23:06 +0530

    Create SpeakHello.js

---
+++ mod_4/SpeakHello.js
@@ -0,0 +1,11 @@
+(function(window){
+ var speakword="Hello";
+ var helloSpeaker={};
+ 
+	    helloSpeaker.speak = function(name){
+		console.log(speakword+" "+name);
+	}
+    window.helloSpeaker=helloSpeaker;	
+}
+)(window);
+
