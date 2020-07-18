a597469d9dc5b26946e662f0d615672c3fa49423
Sahil Sidhani <67865192+Sahil7350@users.noreply.github.com>
2020-07-18 18:23:55 +0530

    Create SpeakGoodBye.js

---
+++ mod_4/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+ (function(window){
+ var speakword = "Good Bye";
+ var byeSpeaker={};
+	 byeSpeaker.speak=function(name){
+	 console.log(speakword+" "+name);
+	};
+    window.byeSpeaker=byeSpeaker;	
+})(window);
+
+
+
